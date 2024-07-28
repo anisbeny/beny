808096fa4aeae10736f4a59e53525757a47006cd
anisbeny <86728842+anisbeny@users.noreply.github.com>
2024-07-28 20:18:38 +0200

    update 1

--- archive.php
+++ archive.php
@@ -1,32 +1,52 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Beny
+ */
+
+get_header();
+?>
+
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-        <h1>Articles</h1>
+        <?php if ( have_posts() ) : ?>
 
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <header class="page-header">
+                <?php
+                the_archive_title( '<h1 class="page-title">', '</h1>' );
+                the_archive_description( '<div class="archive-description">', '</div>' );
+                ?>
+            </header><!-- .page-header -->
 
-                <article class="post">
-              
-                    <h2><?php the_title(); ?></h2>
+            <?php
+            /* Start the Loop */
+            while ( have_posts() ) :
+                the_post();
 
-                    <?php the_post_thumbnail(); ?>
+                /*
+                 * Include the Post-Type-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                 */
+                get_template_part( 'template-parts/content', get_post_type() );
 
-                    <p class="post__meta">
-                        Publié le <?php the_time(get_option('date_format')); ?>
-                        par <?php the_author(); ?>
-                    </p>
+            endwhile;
 
-                    <?php the_excerpt(); ?>
+            the_posts_navigation();
 
-                    <p>
-                        <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-                    </p>
-                </article>
+        else :
 
-        <?php endwhile;
-        endif; ?>
- 
-</main>
-<?php get_template_part( 'template-parts/pagination' ); ?>
-</div>
-<?php get_footer(); ?>
+            get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- template-parts/pagination.php
+++
@@ -1,9 +0,0 @@
-    
- <div class="site__navigation">
-	<div class="site__navigation__prev">
-		<?php previous_posts_link('Page Précédente'); ?>
-    </div>
-    <div class="site__navigation__next">
-        <?php next_posts_link('Page Suivante'); ?> 
-    </div>
-</div>
